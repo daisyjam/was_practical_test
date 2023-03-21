a1b580d51cedf105be23e0e93fe2cd9487b905da
Daisy Jamieson <daisy-jamieson@hotmail.co.uk>
2023-03-21 15:59:26 +0000

    Create models

--- schoolNews/models.py
+++ schoolNews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)
+    date = models.DateTimeField(auto_now=False, auto_now_add=False,)
+    text = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'News'
+
+    def __str__(self):
+        return self.title
